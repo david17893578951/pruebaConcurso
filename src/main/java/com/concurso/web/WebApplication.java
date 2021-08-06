2f309a2df3f4e3982cf009ec9c122f01a892deb1
CuryT1993 <curytravezt.v@gmail.com>
2021-08-06 01:36:33 -0500

    pruba dos

--- src/main/java/com/concurso/web/WebApplication.java
+++ src/main/java/com/concurso/web/WebApplication.java
@@ -9,6 +9,10 @@ public class WebApplication {
 	public static void main(String[] args) {
 		
 		//test de comit en rama Cury
+		
+		
+		
+		//test dos cury travez
 		SpringApplication.run(WebApplication.class, args);
 	}
 
